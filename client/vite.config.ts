3da487cab832c41d0208eb5f31e95fb3b5ad2615
zhangxietao <zhangxt7@chinatelecom.cn>
2025-12-10 17:35:18 +0800

    fix: 优化

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    proxy: {
-      // API 接口
-      '/api': {
-        target: 'http://localhost:3030',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, path.resolve(__dirname, '..'), '');
+  const API_PORT = env.PORT || 3000;
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      proxy: {
+        // API 接口
+        '/api': {
+          target: `http://localhost:${ API_PORT }`,
+          changeOrigin: true,
+        }
       }
     }
   }

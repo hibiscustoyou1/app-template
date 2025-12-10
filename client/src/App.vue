e533ff81ef3eb06373035d5ad6f3827e50741580
zhangxietao <zhangxt7@chinatelecom.cn>
2025-12-10 17:03:48 +0800

    fix: 优化

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <HelloWorld />
 </template>
+
+<script setup lang="ts">
+  import HelloWorld from './components/HelloWorld.vue'
+</script>
 
 <style scoped></style>
